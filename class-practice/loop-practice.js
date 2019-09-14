adf755ca179ffbdf6bdb651b3c30e4bc3ca999bc
jc1989 <jdcannedy@gmail.com>
2019-09-14 13:39:37 -0500

    09/14 2

---
+++ class-practice/loop-practice.js
@@ -0,0 +1,15 @@
+//Practicing loops in class!
+/*
+var class = [student, student, student];
+
+for (var i = 0; i < 10; i++){
+    console.log(class[i].giveAnA())
+    //this loop will run 10 times
+}
+*/
+
+//FOR LOOPS
+for (var i = 0; i < 10; i++){
+    console.log(i);
+}
+
