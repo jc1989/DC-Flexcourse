ae2f307ee1d05de21832bde8902cf71e990fe3c6
Joshua Cannedy <jdcannedy@Joshuas-Air.attlocal.net>
2019-10-13 23:56:11 -0500

    js changes

--- Oct-14-JSdom/JS-DOM/ex8/script.js
+++ Oct-14-JSdom/JS-DOM/ex8/script.js
@@ -4,6 +4,8 @@
     1) Add the ability to check off tasks by clicking on them - (text-decoration:strikethrough is a nice way to)
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
     SUPER BONUS) Make each task editable
+
+    ok
 */
 
 let saveButton = document.getElementById("add-button")
