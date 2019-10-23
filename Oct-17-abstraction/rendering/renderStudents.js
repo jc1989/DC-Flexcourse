63b4945944717ac0a55f1fb7fb5b79bbf4cd8060
Joshua Cannedy <jdcannedy@gmail.com>
2019-10-22 19:03:16 -0500

    rendering exercises

--- Oct-17-abstraction/rendering/renderStudents.js
+++ Oct-17-abstraction/rendering/renderStudents.js
@@ -1,18 +1,20 @@
 
 function renderStudents(students) {
-    let student = "";
-
-    for (i = 0; i < students.length; i++){
-        student += `
-        <div class = "text-center mt-5 nametag">
-        <h1>${students[i].name}</h1>
-        <p>${students[i].isPresent ? "Present" : "Absent"}</p>
+    var studentsHTML = students.map(function(student){
+        return `
+            <div class="m-3 p-3 w-25" style="background-color: ${student.isPresent ? "lightgreen" : "red" };">
+                <h1>${student.name}</h1>
+                <p>${student.isPresent ? "Present" : "Absent"}</p>
+            </div>
+        `
+    });
+    return `
+        <div class="text-center mt-5 d-flex flex-column align-items-center">
+            <h1>Roll Call!</h1>
+            ${studentsHTML.join('')}
         </div>
     `
-    }
-    return student;
 }
-
 
 function students() {
     var content = document.getElementById('content');
--- Oct-22-RenderingState/games/tic-tac-toe/main.js
+++ Oct-22-RenderingState/games/tic-tac-toe/main.js
@@ -10,6 +10,11 @@ content.innerHTML = renderGame(ticTacToe);
 function renderGame(game) {
     // Change this render function to use the "game" parameter
 
+    //X and O positions
+    //Whos turn it is
+    //Who won
+    //Tic-Tac-Toe has 8 winning states // 3 horizontal // 3 vertical // 2 diagonal
+
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
             <h4>It's player O's turn!</h4>
